c290fc033a420b726cb54f64528496737bb731e1
sulaimanalamsyah <102284555+sulaimanalamsyah@users.noreply.github.com>
2024-03-26 12:36:24 +0700

    Tugas 2

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,84 @@
 #include <Arduino.h>
+#include <ESPAsyncWebServer.h>
+#include <ESP8266WiFi.h>
 
-int redLED = D1;
-int yelLED = D0;
-int greLED = D2;
+//password access point
+const char *ap_ssid = "hewan";
+const char *ap_password = "asu12345";
+
+AsyncWebServer server(80);
+String webpage;
+
+int led1 = D0, led2 = D1, led3 = LED_BUILTIN;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
 
 
-void setup() {
-  pinMode(redLED, OUTPUT);
-  pinMode(yelLED, OUTPUT);
-  pinMode(greLED, OUTPUT);
+Serial.begin(9600);
+delay(10);
+
+// setting mode access point
+  Serial.println("Configuring access point...");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ap_ssid, ap_password);
+  Serial.print("Wifi: ");
+  Serial.println(ap_ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  // Isi dari Webpage -------------------------------------------------------
+  webpage+= "<h1> Web Control ESP8266</h1>";
+  webpage+= "<p>LED 1: ";
+  webpage+= "<a href=\"LED1ON\"\"><button>ON</button></a><a href=\"LED1OFF\"\"><button>OFF</button></a></p><br>";
+  webpage+= "<p>LED 2: ";
+  webpage+= "<a href=\"LED2ON\"\"><button>ON</button></a><a href=\"LED2OFF\"\"><button>OFF</button></a></p><br>";
+  webpage+= "<p>LED 3: ";
+  webpage+= "<a href=\"LED3ON\"\"><button>ON</button></a><a href=\"LED3OFF\"\"><button>OFF</button></a></p>";
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", webpage);
+  });
+  
+  server.on("/LED1ON", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(led1, HIGH);
+    request->send(200, "text/html", webpage);
+  });
+  
+  server.on("/LED2ON", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(led2, HIGH);
+    request->send(200, "text/html", webpage);
+  });
+
+  server.on("/LED3ON", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(led3, LOW);
+    request->send(200, "text/html", webpage);
+  });
+
+  server.on("/LED1OFF", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(led1, LOW);
+    request->send(200, "text/html", webpage);
+  });
+  
+  server.on("/LED2OFF", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(led2, LOW);
+    request->send(200, "text/html", webpage);
+  });
+
+  server.on("/LED3OFF", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(led3, HIGH);
+    request->send(200, "text/html", webpage);
+  });
+
+  server.begin();
+
 }
 
-void loop() {
-  digitalWrite(greLED, LOW);
-  digitalWrite(redLED, HIGH);
-  delay(500);
-  digitalWrite(redLED, LOW);
-  digitalWrite(yelLED, HIGH);
-  delay(500);
-  digitalWrite(yelLED, LOW);
-  digitalWrite(greLED, HIGH);
-  delay(500);
+
+void loop()
+{
+ 
 }
